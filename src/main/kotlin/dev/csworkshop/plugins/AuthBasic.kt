24707cd783d892030ef5fe2d48c1be781adf75e7
t1r <>
2022-12-12 11:35:52 +0300

    fix auth logic

--- src/main/kotlin/dev/csworkshop/plugins/AuthBasic.kt
+++ src/main/kotlin/dev/csworkshop/plugins/AuthBasic.kt
@@ -1,10 +1,9 @@
 package dev.csworkshop.plugins
 
-import dev.csworkshop.auth.hashedUserTable
+import dev.csworkshop.auth.AuthService
 import dev.csworkshop.plugins.Constants.PREFIX_API
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -14,15 +13,11 @@ fun Application.authRouting() {
         post("$PREFIX_API/auth") {
             try {
                 val parameters = call.receiveParameters()
-                val name = parameters["name"]!!
-                val password = parameters["password"]!!
+                val name = parameters["name"]
+                val password = parameters["password"]
+                if (name.isNullOrBlank() || password.isNullOrBlank()) throw RuntimeException()
 
-                hashedUserTable.authenticate(
-                    UserPasswordCredential(
-                        name = name,
-                        password = password,
-                    )
-                )
+                AuthService.addCredential(name, password)
                 call.respondText("Auth successful", status = HttpStatusCode.OK)
             } catch (throwable: Throwable) {
                 throwable.printStackTrace()
